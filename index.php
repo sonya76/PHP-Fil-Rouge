a8c8c1beb9e637150bf1315ff904628d94dd0d0f
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-07 14:33:31 +0200

    ajout index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
 date_default_timezone_set('Europe/Paris');
+//include meme erreur il continue alors au require s'arrete
+
+require_once './includes/head.php';
+require_once './includes/main.php';
+require_once './includes/footer.php';

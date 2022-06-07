cce9362c286610eedba5652a2838198eb2ebd817
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-07 16:22:46 +0200

    modification double extension .inc.php

--- includes/main.php
+++ includes/main.php
@@ -4,8 +4,13 @@
 <main>
 <?php
    $files = glob('./includes/*.inc.php');
-   echo "<pre>";
-   var_dump($files);
-   echo "</pre>"; 
+   $page = isset($_GET['page']) ? $_GET['page'] : "accueil";
+   $page = "./includes/" . $page . ".inc.php";
+
+   if(in_array($page, $files))
+   require $page;
+   else
+   require './includes/accueil.inc.php';
 ?>
+
 </main>

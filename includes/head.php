9a5ad01dad18fe5c3783ccfe05102fa131bf0ce6
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-06-14 09:21:40 +0200

    ajout répertoire assets css

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CEPPIC fil rouge</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body>
